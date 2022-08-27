47963274d40e756bb67c8293cf37f8cbbbac7e0e
Taboichebet <taboibrenda@gmail.com>
2022-08-27 17:07:16 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your code here
+const body = document.querySelector("body");
+
+function submitData (uName, uEmail){
+	const userData = {
+		name: uName,
+		email: uEmail
+	};
+	const handleUserData = {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+			Accept: "application/json",
+		},
+		body: JSON.stringify(userData),
+    };
+    fetch("http://localhost:3000/users", handleUserData)
+	  .then(resp => resp.json())
+	   .then(json => {
+		body.append(json.id);
+	})
+	   .catch(function (error) {
+		  alert("A bug detected!!");
+		   body.append(error);
+    });
+    
+}
